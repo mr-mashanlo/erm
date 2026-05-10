9cce348ad7481b1bb693319857585a0b9f9e66d3
mr-mashanlo <mr.mashanlo@yandex.ru>
2026-05-10 11:22:56 +0500

    Update auth middleware and views

--- server/middlewares/is-auth.js
+++ server/middlewares/is-auth.js
@@ -9,11 +9,15 @@ export const isAuth = async ( req, res, next ) => {
       const { id, email, role, accessToken: newAccessToken, refreshToken: newRefreshToken } = await userService.refreshToken( refreshToken );
       res.cookie( 'accessToken', newAccessToken, { maxAge: process.env.COOKIE_ACCESS_TIME, httpOnly: true, sameSite: 'none', secure: true } );
       res.cookie( 'refreshToken', newRefreshToken, { maxAge: process.env.COOKIE_REFRESH_TIME, httpOnly: true, sameSite: 'none', secure: true } );
-      req.user = { id, email, role };
+      const user = { id, email, role };
+      req.user = user;
+      res.locals.currentUser = user;
       return next();
     }
 
-    req.user = tokenService.verifyAccessToken( accessToken );
+    const user = tokenService.verifyAccessToken( accessToken );
+    req.user = user;
+    res.locals.currentUser = user;
     next();
   } catch {
     next( new Unauthorized( [ { name: 'token', message: 'Token not provided' } ] ) );
